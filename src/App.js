8cf4218ca2a75b74a020ac9843dcf726bc24a005
jokale <kalemajoanna@gmail.com>
2021-01-05 15:58:09 +0000

    import and render social media logos

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from 'react';
 import logo from './logo.svg';
 import bgdesktop from './bgdesktop.svg';
 import mockup from './mockup.svg';
-
+import facebook from './facebook.svg'
+import twitter from './twitter.svg'
+import instagram from './instagram.svg'
 class App extends React.Component {
   render(){
       return (
@@ -13,6 +15,9 @@ class App extends React.Component {
       <img src={logo} id="logo" alt="logo" />
       <img src={bgdesktop} id="bgdesktop" alt="logo" />
       <img src={mockup} id="mockup" alt="logo" />
+      <img src={facebook} id="facebook" alt="logo" />
+      {/* <img src={instagram} id="instagram" alt="logo" />
+      <img src={twitter} id="twitter" alt="logo" /> */}
 
     </div>
     )
