00f291987a9ab3ec9ac9d28d1bd559b3baa5e703
jokale <kalemajoanna@gmail.com>
2021-01-05 13:28:09 +0000

    add header and body

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App extends React.Component {
   render(){
       return (
     <div id="App">
-      <h1>Hello</h1>
+      <h1>Build The Community <br></br> Your Fans Will Love</h1>
+      <p>Huddle re-imagines the way we build communities. You have <br></br> a voice, but so does your audience. Create connections with <br></br> your users as you engage in geninune discussion</p>
     </div>
     )
   }
