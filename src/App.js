3348f9b07ada402a06fb7aa3c18d7fde64c6d353
jokale <kalemajoanna@gmail.com>
2021-01-05 13:55:17 +0000

    position text and huddle logo

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
     <div id="App">
       <h1>Build The Community <br></br> Your Fans Will Love</h1>
       <p>Huddle re-imagines the way we build communities. You have <br></br> a voice, but so does your audience. Create connections with <br></br> your users as you engage in geninune discussion</p>
-      <img src={logo} id="slogo" alt="logo" />
+      <img src={logo} id="logo" alt="logo" />
       {/* <img src={bgdesktop} id="bgdesktop" alt="logo" /> */}
       {/* <img src={mockup} id="mockup" alt="logo" /> */}
 
