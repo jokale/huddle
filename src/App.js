f8a67cc2cea0102fb93aad2ed11b65f0d3de14af
jokale <kalemajoanna@gmail.com>
2021-01-05 14:06:51 +0000

    reimport background and image

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ class App extends React.Component {
       <h1>Build The Community <br></br> Your Fans Will Love</h1>
       <p>Huddle re-imagines the way we build communities. You have <br></br> a voice, but so does your audience. Create connections with <br></br> your users as you engage in geninune discussion</p>
       <img src={logo} id="logo" alt="logo" />
-      {/* <img src={bgdesktop} id="bgdesktop" alt="logo" /> */}
-      {/* <img src={mockup} id="mockup" alt="logo" /> */}
+      <img src={bgdesktop} id="bgdesktop" alt="logo" />
+      <img src={mockup} id="mockup" alt="logo" />
 
     </div>
     )
